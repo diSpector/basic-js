3de679efe0b0d6bd118b85e5f0bbf2ce38ef7366
Dmitriy Semenov <semendm1@icnx.ru>
2020-03-05 19:05:24 +0300

    add: correct hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     turns = Math.pow(2, disksNumber) - 1;
-    seconds = turns / turnsSpeed * 3600;
+    seconds = turns / (turnsSpeed / 3600);
     return { turns, seconds }
 }
